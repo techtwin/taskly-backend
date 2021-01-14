10d081202721686e224403ad72743b0dc7156115
techtwins <gshevaughn@gmail.com>
2021-01-14 13:26:40 -0500

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,6 +12,7 @@ Task.destroy_all
 FriendRequest.destroy_all
 User.destroy_all
 
+
 puts "Creating Users 👨🏽"
 user_arr = [
   {
@@ -44,18 +45,61 @@ user_arr.each do |userObj|
   User.create!(userObj)
 end
 
+
 puts "Creating Lists 📝"
 list_arr = [
   {
+    user_id: User.first.id,
     name: "Study",
     color: "#39F014"
   }
   {
+    user_id: User.first.id,
     name: "Important",
     color: "#FF1700"
   }
   {
+    user_id: User.third.id,
     name: "Health",
     color: "#FF75FB"
   }
+  {
+    user_id: User.second.id,
+    name: "Entertainment",
+    color: "#07E5FF"
+  }
 ]
+
+list_arr.each do |listObj|
+  List.create!(listObj)
+end
+
+
+puts "Creating Tasks 📌"
+task_arr = [
+  {
+    list_id: List.first.id,
+    name: "Data Structures & Algos",
+    description: "Study these so I can be good at backend sht and enjoy the smart life",
+    date: "01-15-2021",
+    time: "10:30AM"
+  }
+  {
+    list_id: List.first.id,
+    name: "SCSS",
+    description: "Study some SCSS",
+    date: "04-25-2021",
+    time: "11:10PM"
+  }
+  {
+    list_id: List.first.id,
+    name: "Typescript",
+    description: "Do some work in Typescript",
+    date: "02-09-2020",
+    time: "8:00AM"
+  }
+]
+
+task_arr.each do |taskObj|
+  Task.create!(taskObj)
+end
