17f633840ecd5b166eb0d6a245858f598315e984
techtwins <gshevaughn@gmail.com>
2021-01-14 09:43:28 -0500

    added actions to the user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,35 @@
 class UsersController < ApplicationController
+
+  def index 
+    user = User.all
+    render json: users
+  end
+
+  def new 
+    user = User.new
+    render json: user
+  end
+
+  def create
+    user = User.create!(user_params)
+    render json: user
+  end
+
+  def update
+    user = User.find(params[:id])
+    user.update!(user_params)
+    render json: user
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.destroy!
+    render json: {}
+  end
+
+  private
+
+  def user_params 
+    params.permit(:username, :password, :name, :img)
+  end
 end
