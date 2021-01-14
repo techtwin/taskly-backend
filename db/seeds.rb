0224d4f38e5865908b87efed4ddd233a9b2805d5
techtwins <gshevaughn@gmail.com>
2021-01-14 13:28:37 -0500

    successfully seeded data

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,25 +20,25 @@ user_arr = [
     password: "123",
     name: "Shevaughn Grant",
     img: "https://i.imgur.com/RqIw2b4.png"
-  }
+  },
   {
     username: "jaquan1314",
     password: "123",
     name: "Javaughn Grant",
     img: "https://i.imgur.com/RqIw2b4.png"
-  }
+  },
   {
     username: "juliaz",
     password: "123",
     name: "Julia Zolotarev",
     img: "https://i.imgur.com/0iSQ15t.png"
-  }
+  },
   {
     username: "steven",
     password: "123",
     name: "Steven Wu",
     img: "https://i.imgur.com/a7TJpiA.png"
-  }
+  },
 ]
 
 user_arr.each do |userObj|
@@ -52,22 +52,22 @@ list_arr = [
     user_id: User.first.id,
     name: "Study",
     color: "#39F014"
-  }
+  },
   {
     user_id: User.first.id,
     name: "Important",
     color: "#FF1700"
-  }
+  },
   {
     user_id: User.third.id,
     name: "Health",
     color: "#FF75FB"
-  }
+  },
   {
     user_id: User.second.id,
     name: "Entertainment",
     color: "#07E5FF"
-  }
+  },
 ]
 
 list_arr.each do |listObj|
@@ -83,21 +83,21 @@ task_arr = [
     description: "Study these so I can be good at backend sht and enjoy the smart life",
     date: "01-15-2021",
     time: "10:30AM"
-  }
+  },
   {
     list_id: List.first.id,
     name: "SCSS",
     description: "Study some SCSS",
     date: "04-25-2021",
     time: "11:10PM"
-  }
+  },
   {
     list_id: List.first.id,
     name: "Typescript",
     description: "Do some work in Typescript",
     date: "02-09-2020",
     time: "8:00AM"
-  }
+  },
 ]
 
 task_arr.each do |taskObj|
