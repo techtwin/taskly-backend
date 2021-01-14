8499c5866c5e8fff9e2179e73fde46e9721cbe1c
techtwins <gshevaughn@gmail.com>
2021-01-14 13:35:38 -0500

    user controller and serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,5 +2,6 @@ class UserSerializer < ActiveModel::Serializer
   attributes :id, :username, :password, :name, :img
   has_many :lists
   has_many :collaborators
-  has_many :friend_requests
+  has_many :friend_requests_as_receiver
+  has_many :friend_requests_as_requestor
 end
