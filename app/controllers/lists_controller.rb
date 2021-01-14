f3adcea29877385980d711c746a69cb312fcc073
techtwins <gshevaughn@gmail.com>
2021-01-14 10:05:41 -0500

    updated lists controller

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,39 @@
 class ListsController < ApplicationController
+
+  def index 
+    lists = List.all
+    render json: lists
+  end
+
+  def new 
+    list = List.new
+    render json: list
+  end
+
+  def show
+    list = List.find(params[:id])
+  end
+
+  def create
+    list = List.create!(list_params)
+    render json: list
+  end
+
+  def update
+    list = List.find(params[:id])
+    list.update!(list_params)
+    render json: list
+  end
+
+  def destroy
+    list = List.find(params[:id])
+    list.destroy!
+    render json: {}
+  end
+
+  private
+
+  def list_params 
+    params.permit(:name, :color)
+  end
 end
