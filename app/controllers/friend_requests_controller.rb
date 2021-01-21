04e41de8542f3d8d14fd24b931902ecfa4fe3c2f
techtwins <gshevaughn@gmail.com>
2021-01-21 00:55:13 -0500

    updated friendrequests controller & serializer

--- app/controllers/friend_requests_controller.rb
+++ app/controllers/friend_requests_controller.rb
@@ -1,7 +1,26 @@
 class FriendRequestsController < ApplicationController
 
-  def index
-    friendrequests = FriendRequest.all
-    render json: friendrequests
+  # def index
+  #   friendrequests = FriendRequest.all
+  #   render json: friendrequests
+  # end
+
+  def create
+    friendrequest = FriendRequest.create!(friendrequest_params)
+    render json: { friendrequest: FriendRequestSerializer.new(friendrequests)}, status: :accepted
+  end
+
+  def destroy 
+    arr = params[:id].split("&")
+    requestor_id = arr[0]
+    receiver_id = arr[1]
+    request = FriendRequest.find_by(requestor_id: requestor_id, receiver_id: receiver_id)
+    request.destroy
+  end
+
+  private
+
+  def friendrequest_params 
+    params.permit(:requestor_id, :receiver_id)
   end
 end
--- app/serializers/friend_request_serializer.rb
+++ app/serializers/friend_request_serializer.rb
@@ -1,3 +1,11 @@
 class FriendRequestSerializer < ActiveModel::Serializer
   attributes :id, ,:receiver_id, :requestor_id, :status
+
+  def receiver
+    {
+    id: self.object.receiver.id,
+    name: self.object.receiver.name,
+    img: self.object.receiver.img
+    }
+  end
 end
