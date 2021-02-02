956ad0d09b22d24c79946a18355f0f2e30adaa30
techtwins <gshevaughn@gmail.com>
2021-02-02 18:02:05 -0500

    updated user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,6 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :username, :password, :name, :img, :requests_sent, :requests_received
-  # has_many :lists
+  has_many :lists
   # has_many :collaborators
   # has_many :friend_requests_as_receiver
   # has_many :friend_requests_as_requestor
