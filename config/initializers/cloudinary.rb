d92423a16f355479c5658af69c0db161fb9e8521
techtwins <gshevaughn@gmail.com>
2021-02-09 16:52:45 -0500

    set up cloudinary file

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,7 @@
+Cloudinary.config do |config|    
+  config.cloud_name = "techtwin"   
+  config.api_key = "554455793537949"   
+  config.api_secret = "YpJrBHptL4fPsjK6J1rpSlkpjWw"   
+  config.secure = true    
+  config.cdn_subdomain = true  
+end
